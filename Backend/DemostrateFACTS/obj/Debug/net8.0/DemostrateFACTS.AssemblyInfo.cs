0a25c770af6ca741b275d9f77f688a3dacea956a
Enzo He <enzoenzohe@gmail.com>
2024-10-12 07:53:54 +0800

    .

--- Backend/DemostrateFACTS/obj/Debug/net8.0/DemostrateFACTS.AssemblyInfo.cs
+++ Backend/DemostrateFACTS/obj/Debug/net8.0/DemostrateFACTS.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DemostrateFACTS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f15a0bfbff6db4e8adc8f9e84303c2d93ebd861")]
 [assembly: System.Reflection.AssemblyProductAttribute("DemostrateFACTS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DemostrateFACTS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
